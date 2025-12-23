0bbc816055f9aac9aa7d073424714acef7f33b45
Your Name <you@example.com>
2025-12-23 23:35:51 +0700

    Fix backend db connection and chat 401

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -36,9 +36,9 @@ export default function Landing() {
             </div>
 
             <h1 className="text-5xl md:text-7xl font-bold tracking-tight text-white font-sans mb-8 leading-tight">
-              basis data untuk <br />
+              WANZDB X<br />
               <span className="text-transparent bg-clip-text bg-gradient-to-r from-sky-400 to-cyan-300">
-                aplikasi masa depan
+                WANZOFC 
               </span>
             </h1>
             
